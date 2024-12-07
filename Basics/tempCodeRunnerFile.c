f974018dbc548844479038df601798f35b9262a7
Kallol Das Kushol <kallold70@gmail.com>
2024-12-07 23:44:12 +0600

    normal

--- Basics/tempCodeRunnerFile.c
+++ Basics/tempCodeRunnerFile.c
@@ -1,9 +1,24 @@
-    int i;
-    printf("Enter pattern:");
-    scanf("%d",&i);
-    for(int row=1;row<=i;row++){
-        for(int col=i;col>=row;col--){
-            printf("* ");
+#include<stdio.h>
+
+int main(){
+    // int i,j,k=1;
+    // for( i=5;i>=2;i--){
+    //     for( j=1;j<=2;j++){
+    //         for(;k<=10;k=k*4){
+    //             printf("%d %d %d \n",i,j,k);
+    //         }
+    //     }
+    // }
+    // printf("%d %d %d ",i,j,k);
+    for(int p=1;p<2;p++){
+        for(int s=0;s<=1;s++){
+            for(int q=-1;q<1;q++){
+                for(int r=-11;r<=13;r+=5){
+                    printf("%d %d %d %d \n",p,q,r,s);
+                }
+            }
         }
-    printf("\n");
     }
+
+return 0;
+}
