b6ec2ac1a78a436275d97cfa4ffe311810deb632
Kallol Das Kushol <kallold70@gmail.com>
2025-01-28 23:32:45 +0600

    normal

--- Project_1.c
+++ Project_1.c
@@ -2,8 +2,7 @@
 #include <string.h>
 struct student
 {
-    char name[200];
-    char department[100];
+    char name[100];
     int batch;
     int id;
 };
@@ -11,19 +10,102 @@ struct student
 struct student CSE[max_Student];
 int studentCount = 0;
 
+void addStudent(){
+    if(studentCount >= max_Student){
+        printf("Student limit Crossed\n");
+        printf("****RETURNING TO THE ADMIN PORTAL****\n\n");
+        return;
+    }
+    
+        printf("Enter Student Name:");
+        fgets(CSE[studentCount].name, sizeof(CSE[studentCount].name), stdin);
+        CSE[studentCount].name[strcspn(CSE[studentCount].name, "\n")] = '\0';
+
+        printf("Enter student id: ");
+        scanf("%d", &CSE[studentCount].id);
+        
+        printf("Enter batch: ");
+        scanf("%d", &CSE[studentCount].batch);
+
+        printf("Student Added Successfully! Welcome %s to LU <3\n", CSE[studentCount].name);   
+        printf("****RETURNING TO THE ADMIN PORTAL****\n\n");
+
+        studentCount++;
+    }
+
+void displayStudent(){
+        if(studentCount == 0 ){
+            printf("No record found\n");
+            printf("****RETURNING TO THE ADMIN PORTAL****\n\n");
+            return;
+        }
+        else {
+            printf("\nStudent Records: \n");
+            for(int i=0; i<studentCount; i++){
+                printf("Student number: %d\n", (i+1));
+                printf("Name: %s \n", CSE[i].name);
+                printf("ID: %d \n", CSE[i].id);
+                printf("Batch: %d \n", CSE[i].batch);
+                printf("****RETURNING TO THE ADMIN PORTAL****\n\n");
+            }
+        }
+    }
+
+    void searchStudent(){
+        int id;
+        printf("Enter the id of the student:\n");
+        scanf("%d", &id);
+        getchar();
+
+        for(int i=0; i<studentCount; i++){
+            if(id == CSE[i].id){
+                printf("Student FOUND!!!!!\n");
+                printf("Name: %s \n", CSE[i].name);
+                printf("ID: %d \n", CSE[i].id);
+                printf("Batch: %d \n", CSE[i].batch);
+                printf("****RETURNING TO THE ADMIN PORTAL****\n\n");
+                return;
+            }
+            else{
+                printf("Student Not Found\n");
+                printf("****RETURNING TO THE ADMIN PORTAL****\n\n");
+
+            }
+        }
+    }
+    void deleteStudent(){
+        int dlt;
+        printf("Enter the id of the student you want to delete:");
+        scanf("%d", &dlt);
+        for(int i=0; i<studentCount; i++){
+            if(CSE[i].id == dlt){
+                for(int j=dlt; j<studentCount; j++){
+                    CSE[j].id = CSE[j+1].id;
+                }
+                printf("Delete successfull!!!!\n");
+                studentCount--;
+                printf("****RETURNING TO THE ADMIN PORTAL****\n\n");
+            }else{
+                printf("Student not found :( \n");
+                printf("****RETURNING TO THE ADMIN PORTAL****\n\n");
+            }
+
+        }
+    }
 void menu()
 {
     int choice;
     do
     {
-        printf("Welcome to the menu of Leading University Student Portal\n");
+        printf("\n****Welcome to the menu of Leading University Student Portal****\n");
         printf(" 1. Add Student\n");
         printf(" 2. Display All Student\n");
         printf(" 3. Search Student\n");
         printf(" 4. Delete Student\n");
         printf(" 5. Logout\n");
 
-        printf("%d", &(choice));
+        scanf("%d", &choice);
+        getchar();
 
         switch (choice)
         {
@@ -48,27 +130,6 @@ void menu()
     }while (choice != 5);
 }
 
-void addStudent(){
-    if(studentCount >= max_Student){
-        printf("Student limit Crossed\n");
-        return;
-    }
-    else{
-        printf("Enter Student Name: ");
-        fgets(CSE[studentCount].name, sizeof(CSE[studentCount].name), stdin);
-
-        CSE[studentCount].name[strcspn(CSE[studentCount].name, "\n")] = '\0';
-
-        printf("Enter student id: ");
-        scanf("%d", &CSE[studentCount].id);
-
-        printf("Enter batch: ");
-        scanf("%d", &CSE[studentCount].batch);
-
-        printf("Student Added Successfully! Welcome %s to LU \n", CSE[studentCount].name);
-        studentCount++;
-    }
-}
     int main()
     {
 
@@ -89,12 +150,12 @@ void addStudent(){
 
         if (checkPass == 0 && checkUser == 0)
         {
-            printf("Congo! Login Successfull :)\n");
+            printf("\nCongo! Login Successfull :)\n");
             menu();
         }
         else
         {
-            printf("Invaild login ;) \n");
+            printf("\nInvaild login ;) \n");
         }
         return 0;
     }
